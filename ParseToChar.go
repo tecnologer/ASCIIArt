f099053344b34b2d79bea81da714c5e630b3ea4e
Rey David Dominguez <rdominguez@tecnologer.net>
2017-10-05 23:59:49 -0700

    Print with background of characters

--- ParseToChar.go
+++ ParseToChar.go
@@ -37,6 +37,63 @@ func Parsef(word string, sign rune) string {
 	return finalWord
 }
 
+func ParsefBackground(word string, sign rune, background rune) string {
+	word = strings.ToUpper(word)
+	word = removeSpecialChars(word)
+
+	var finalWord string
+	var lenght int
+	for row := 0; row < 7; row++ {
+		for _, char := range word {
+			if letter, ok := Fonts.BoldFont[char]; ok {
+				letterPart := letter[row]
+				if row == 0 {
+					lenght += len(letterPart)
+				}
+
+				finalWord += replace(letterPart, sign, background)
+			}
+		}
+		finalWord += "\r\n"
+	}
+
+	return addHeaderFooter(finalWord, background, lenght)
+}
+
+func replace(letterPart string, char rune, bkg rune) string {
+	var spce, _ = regexp.Compile(`\s`)
+	var strks, _ = regexp.Compile(`\*`)
+
+	if char == ' ' || bkg == '*' {
+		letterPart = spce.ReplaceAllString(letterPart, "x")
+		letterPart = strks.ReplaceAllString(letterPart, string(char))
+		var special, _ = regexp.Compile(`x`)
+		letterPart = special.ReplaceAllString(letterPart, string(bkg))
+	} else {
+		if char != '*' && letterPart != "" {
+			var regex, _ = regexp.Compile(`\*`)
+			letterPart = regex.ReplaceAllString(letterPart, string(char))
+		}
+
+		//reemplace white spaces to specific char
+		if bkg != ' ' && bkg != char {
+			var regex, _ = regexp.Compile(`\s`)
+			letterPart = regex.ReplaceAllString(letterPart, string(bkg))
+		}
+	}
+
+	return letterPart
+}
+
+func addHeaderFooter(finalWord string, bkg rune, lenght int) string {
+	var HF string
+	for i := lenght; i > 0; i-- {
+		HF += string(bkg)
+	}
+
+	return HF + "\r\n" + finalWord + HF
+}
+
 //ParsefNExport creates a file with the parsed to letters formed with specific sign
 func ParsefNExport(text string, sign rune) {
 	dir, err := os.Getwd()
